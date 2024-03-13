f288718741d723772be89a8311a2aab71b26efd0
Mikhail Demichev <demichev.mi@gmail.com>
2024-03-13 16:36:50 +0700

    Initial Commit

---
+++ Expenses_App/Expenses_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expenses_AppApp.swift
+//  Expenses_App
+//
+//  Created by Mikhail Demichev on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Expenses_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
